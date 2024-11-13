fb2ac763491758b8cc9244b6ee58bbac9a8055f5
Charalampos <charavant@gmail.com>
2024-11-13 16:57:13 +0200

    .

---
+++ Day16/main.py
@@ -0,0 +1,18 @@
+# from turtle import Turtle,Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+
+# ##triagle
+# timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
