17293d8e1219db839b0a8353e47bca464328965f
Charalampos <charavant@gmail.com>
2024-11-19 16:51:41 +0200

    .

--- Day16/main.py
+++ Day16/main.py
@@ -16,3 +16,15 @@
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
 
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikatsu","Squirtle","Charmander"])
+# table.add_column("Type", ["Electric","Water","Fire"])
+
+# print(table)
+
+
+from menu import Menu, MenuItem
+from coffer_maker import CoffeeMaker
+from money_machine import MoneyMachine
